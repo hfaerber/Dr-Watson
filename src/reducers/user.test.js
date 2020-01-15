43f87538333718e2290f8f9b672538b1058b1865
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-15 11:56:51 -0700

    Add tests for user reducer

--- src/reducers/user.test.js
+++ src/reducers/user.test.js
@@ -7,4 +7,34 @@ describe('user', () => {
     expect(result).toEqual(expected);
   });
 
+  it('should return a user object when type is CREATE_USER', () => {
+    const mockUser = {id: 1579112830201,
+      firstName:"ben",
+      lastName:"beans",
+      feeling:"happy"
+    };
+    const testState = null;
+    const action = {
+      type: 'CREATE_USER',
+      user: mockUser
+    };
+    const expected = mockUser;
+    const result = user(testState, action);
+    expect(result).toEqual(expected);
+  })
+
+  it('should return null when type is REMOVE_USER', () => {
+    const mockUser = {id: 1579112830201,
+      firstName:"ben",
+      lastName:"beans",
+      feeling:"happy"
+    };
+    const testState = mockUser;
+    const action = {
+      type: 'REMOVE_USER'
+    };
+    const expected = null;
+    const result = user(testState, action);
+    expect(result).toEqual(expected);
+  })
 })
