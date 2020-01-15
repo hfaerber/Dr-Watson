1d19e88fba86ee51ec5abf3c092615303bb16993
Heather Faerber <heatherdfaerber@gmail.com>
2020-01-15 11:51:52 -0700

    Add second test for errorMsg reducer

--- src/reducers/errorMsg.test.js
+++ src/reducers/errorMsg.test.js
@@ -6,4 +6,16 @@ describe('errorMsg', () => {
     const result = errorMsg(undefined, {});
     expect(result).toEqual(expected);
   });
+
+  it('should return an error message when type is HAS_ERRORED', () => {
+    const testState = '';
+    const action = {
+      type: 'HAS_ERRORED',
+      errorMsg: 'test error'
+    };
+    const expected = 'test error';
+    const result = errorMsg(testState, action);
+    expect(result).toEqual(expected);
+  })
+
 })
